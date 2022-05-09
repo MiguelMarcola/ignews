8808acbdcb059613df08d388de249990fd1b5dc0
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-09 17:19:23 -0300

    create sripe id in faudadb

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,15 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+    ref: {
+        id: string;
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         const session = await getSession({ req })
 
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index("users_by_email"),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
         const stripeCustumer = await stripe.customers.create({
             email: session.user.email,
             // metadata
         })
+
+        await fauna.query(
+            q.Update(
+                q.Ref(q.Collection("users"), user.ref.id),
+                {
+                    data: {
+                        stripe_customer_id: stripeCustumer.id,
+                    }
+                }
+            )
+        )
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
             customer: stripeCustumer.id,
