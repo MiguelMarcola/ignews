061c94773aea2ee2665e20261bd7ed32cb345711
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-08 23:51:30 -0300

    validate if user alredy exists

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -23,14 +23,31 @@ export default NextAuth({
 
             try {
                 await fauna.query(
-                    q.Create(
-                        q.Collection('users'),
-                        { data: { email } }
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index("users_by_email"),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index("users_by_email"),
+                                q.Casefold(user.email)
+                            )
+                        )
                     )
                 )
 
                 return true;
-            } catch {
+            } catch (err) {
+                console.log(err)
                 return false;
             }
         },
