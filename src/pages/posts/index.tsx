fc4cdda7b788f39bb92d541470114b586e600a95
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-11 17:18:30 -0300

    config prismic API

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,8 @@
 import styles from "./styles.module.scss";
 import Head from "next/head";
+import Prismic from "@prismicio/client";
+import { GetStaticProps } from "next";
+import { getPrismicClient } from "../../services/prismic";
 
 export default function Posts() {
     return (
@@ -29,4 +32,21 @@ export default function Posts() {
             </main>
         </>
     );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const prismic = getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at("document.type", "post")
+    ], {
+        fetch: ["post.Title", "post.Content"],
+        pageSize: 100,
+    })
+
+    console.log(JSON.stringify(response, null, 2))
+
+    return {
+        props: {}
+    }
 }
