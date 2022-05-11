f2e19943107fa84cad8302c2e04ced69d3f4cc80
MiguelMarcola <miguelmarcola@gmail.com>
2022-05-11 19:47:23 -0300

    consuming data from pismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,23 @@
 import styles from "./styles.module.scss";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom"
 import { GetStaticProps } from "next";
 import { getPrismicClient } from "../../services/prismic";
 
-export default function Posts() {
+type Posts = {
+    slug: string;
+    title: string;
+    exerpt: string;
+    updatedAt: string;
+}
+
+interface PostsProps {
+    posts: Posts[];
+
+}
+
+export default function Posts({ posts }: PostsProps) {
     return (
         <>
             <Head>
@@ -13,21 +26,14 @@ export default function Posts() {
 
             <main className={styles.container}>
                 <div className={styles.posts}>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa saepe voluptatibus provident? Animi exercitationem officiis itaque mollitia ullam provident, quasi vel nulla corrupti. Id dolores qui ipsam officia sunt?</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa saepe voluptatibus provident? Animi exercitationem officiis itaque mollitia ullam provident, quasi vel nulla corrupti. Id dolores qui ipsam officia sunt?</p>
-                    </a>
-                    <a href="#">
-                        <time>12 de março de 2021</time>
-                        <strong>Creating bla bla</strong>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsa saepe voluptatibus provident? Animi exercitationem officiis itaque mollitia ullam provident, quasi vel nulla corrupti. Id dolores qui ipsam officia sunt?</p>
-                    </a>
+                    {posts.map(post => (
+                        <a href="#" key={post.slug}>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.exerpt}</p>
+                        </a>
+                    )
+                    )}
                 </div>
             </main>
         </>
@@ -37,16 +43,26 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
     const prismic = getPrismicClient()
 
-    const response = await prismic.query([
+    const response = await prismic.query<any>([
         Prismic.predicates.at("document.type", "post")
     ], {
         fetch: ["post.Title", "post.Content"],
         pageSize: 100,
     })
 
-    console.log(JSON.stringify(response, null, 2))
-
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.Title),
+            exerpt: post.data.Content.replaceAll("\n", " ").split(" ").slice(0, 30).join(" ").concat("..."),
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+                day: "2-digit",
+                month: "long",
+                year: "numeric"
+            })
+        }
+    })
     return {
-        props: {}
+        props: { posts }
     }
 }
