12be839ec456eeb882e0ae54574ba916634ec6d5
MiguelMarcola <miguelmarcola@gmail.com>
2022-06-03 10:15:45 -0300

    advanced with tests

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react"
+import { Header } from "."
+
+jest.mock("next/router", () => {
+    return {
+        useRouter() {
+            return {
+                asPath: "/home"
+            }
+        }
+    }
+})
+
+jest.mock("next-auth/react", () => {
+    return {
+        useSession() {
+            return [null, false]
+        }
+    }
+})
+
+describe("Header component", () => {
+
+    it("renders correctly", () => {
+        render(
+            <Header />
+        )
+
+        expect(screen.getByText("Home")).toBeInTheDocument();
+        expect(screen.getByText("Posts")).toBeInTheDocument();
+    })
+
+})
+
+
